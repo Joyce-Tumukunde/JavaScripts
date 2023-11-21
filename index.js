b6a828331747021c096c19d6435021197ad632bb
joyce-tumukunde <jtumukunde60@gmail.com>
2023-11-21 06:21:36 -0800

    my first javascript codes

---
+++ index.js
@@ -0,0 +1,35 @@
+//Addition of two number
+/*const a = 7;
+var b =5;
+var c = a+b;
+console.log(c);*/
+
+//Increment number
+/*var a = 10;
+a++;
+console.log(a);*/
+
+/*let a = 20;
+a += 40;
+console.log(a);*/
+
+/*const a = "This is the start.\n " + "This is the end."; // Change this line
+console.log(a);
+
+let b ="FirstLine\n\tSecondLine\nThirdLine\n\tFouthLine.";
+console.log(b)
+
+console.log("Hello, World");*/
+
+//understanding unitialized variable
+
+var a = 6;
+var b = 15;
+var c ="I am a ";
+a += 1;
+b += 5;
+c += "string";
+console.log(a,b,c);
+//console.log(b);
+//console.log(c);
+
