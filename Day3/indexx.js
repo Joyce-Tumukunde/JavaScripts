c5e6e48052e8893bb45a602ee5ce833bb2454625
joyce-tumukunde <jtumukunde60@gmail.com>
2023-11-27 06:42:45 -0800

    my portifolio

---
+++ Day3/indexx.js
@@ -0,0 +1,10 @@
+let  height  =  180;
+let  anotherHeight  =  height;
+let  weight;
+console.log(height); 
+console.log(anotherHeight);  
+weight  =  70;    
+console.log(weight);
+
+const  greeting  =  "Hello!";
+console.log(greeting)
